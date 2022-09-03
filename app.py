61cc2e6d5f110c2598060525ed5b26701e11f432
nickrally <nickrally2009@gmail.com>
2022-09-03 13:57:45 -0600

    full crud

--- app.py
+++ app.py
@@ -1,35 +1,86 @@
-from flask import Flask, request, render_template
+import os
+from flask import Flask, request, render_template, redirect, url_for
 from wtforms import Form, StringField,  Field, validators
 from wtforms.validators import InputRequired, Length
 from flask_wtf import FlaskForm
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import or_ , and_
+from sqlalchemy.exc import IntegrityError, DataError
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
-app.secret_key = b'ZsbK\x9fdD|`\x07\x05\x01\x95\x93u\xae'
+app.secret_key = 'dessertafterdinner'
 
-class AddressForm(FlaskForm):
-    name     = StringField('name')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+db = SQLAlchemy(app)
+
+
+class PlaceForm(FlaskForm):
+    name = StringField('name')
     street_address = StringField('street_address')
-    city    = StringField('city')
+    city = StringField('city')
     state = StringField('state')
-    zipcode = StringField('zipcode', validators=[InputRequired(),
-                                            Length(5)])
+    zip = StringField('zip', validators=[InputRequired(), Length(5)])
 
 
-# @app.route('/')
-# def index():
-#     return render_template('home.html')
+class Place(db.Model):
+    __tablename__ = 'dessert_after_meal'
+    __searchable__ = ['name']
 
-@app.route('/', methods=['GET','POST'])
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text,    unique=False, nullable=False)
+    street_address =db.Column(db.Text, unique=False, nullable=False)
+    city = db.Column(db.Text, unique=False, nullable=True)
+    state = db.Column(db.Text, unique=True, nullable=False)
+    zip = db.Column(db.Text, unique=True, nullable=False)
+
+    def __init__(self, name=name, street_address=street_address, city=city, state=state, zip=zip):
+        self.name = name
+        self.street_address  = street_address
+        self.city = city
+        self.state = state
+        self.zip = zip
+
+
+@app.route('/', endpoint='home', methods=['GET','POST'])
 def index():
-    form = AddressForm(request.form)
-    if request.method == 'POST' and form.validate():
-        return render_template('home.html')
-    return render_template('home.html', form=form)
+    places = Place.query.order_by(Place.id.desc())
+    return render_template('home.html', places=places)
 
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        place = Place(request.form['name'], request.form['street_address'], request.form['city'],
+                         request.form['state'], request.form['zip'])
+
+        db.session.add(place)
+        db.session.commit()
+        return redirect(url_for('home'))
+    return render_template('add.html')
+
+
+@app.route('/edit/<string:id>', methods=['GET','POST'], endpoint='edit')
+def edit(id):
+    place = Place.query.get(id)
+    form = PlaceForm(obj=place)
+    if request.method == 'POST':
+        form.populate_obj(place)
+        db.session.add(place)
+        db.session.commit()
+        return redirect(url_for('home'))
+    return render_template('edit.html', place=place, id=id, form=form)
+
+
+@app.route('/delete/<int:id>', methods=['GET','POST'])
+def delete(id):
+    place = Place.query.get(id)
+    if request.method == 'POST':
+        db.session.delete(place)
+        db.session.commit()
+        return redirect(url_for('home'))
+    return render_template('delete.html', place=place, id=id)
 
 
 if __name__ == "__main__":
